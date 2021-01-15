fe5422cb837e7f6e501badcb49e3814d2d3d9a66
ZoAnderton42 <91346@farnborough.ac.uk>
2021-01-15 13:50:19 +0000

    15/01/21 logouts everywhere

--- resources/client/js/create.js
+++ resources/client/js/create.js
@@ -23,3 +23,23 @@ function createArticle() {
         }
     });
 }
+
+function logout() {
+    debugger;
+    console.log("Invoked logout");
+    let url = "/user/logout";
+    fetch(url, {
+        method: "POST",
+    }).then(response => {
+        return response.json();                 //now return that promise to JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) {
+            alert(JSON.stringify(response));        // if it does, convert JSON object to string and alert
+        } else {
+            Cookies.remove("Token", response.Token);    //Username and Token are removed
+            Cookies.remove("Username", response.Username);
+            Cookies.remove("Admin", response.Admin);
+            window.open("home.html", "_self");       //open home.html in same tab
+        }
+    });
+}
